bd5e90776afd235eed814cda299e0fae0196d2da
Zosman <zach.osman@gmail.com>
2017-01-15 15:44:36 -0500

    lots of stuff

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
 const fs = require('fs');
 const mv = require('mv');
 
-function test(){
-  console.log("hi")
+function test() {
+  																														console.log('hi');
 }
 
 const testFolder = './tests/';
 fs.readdir(testFolder, (err, files) => {
-  files.forEach(file => {
-    console.log(file);
+  																														files.forEach(file => {
+    																														console.log(file);
   });
 });
 
-
-mv('/tests/test1.txt', '/test1.txt', function(err) {
-  console.log("Error in file ")
+mv('/tests/test1.txt', '/test1.txt', function (err) {
+  																														console.log('Error in file ');
 });
 
-mv('/tests/test1.txt', 'test1.txt', function(err) {
-  console.log("Error in file moving")
+mv('/tests/test1.txt', 'test1.txt', function (err) {
+  																														console.log('Error in file moving');
 });
