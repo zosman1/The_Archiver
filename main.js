e16aa42808fe832fd2778b987b793b0de5b597d8
Zosman <zach.osman@gmail.com>
2017-01-15 16:22:05 -0500

    creating test cases to learn how mv works

--- main.js
+++ main.js
@@ -12,10 +12,38 @@ fs.readdir(testFolder, (err, files) => {
   })
 })
 
-mv('/tests/test1.txt', '/test1.txt', function (err) {
-  console.log('Error in file ')
-})
+// mv('/Users/ZachO/Code/The_Archiver/tests/test1.txt', '/Users/ZachO/Code/The_Archiver/test1.txt', function (err) {
+//   console.log('Error in file ')
+// })
 
-mv('/tests/test1.txt', 'test1.txt', function (err) {
-  console.log('Error in file moving')
-})
+// mv('/tests/test1.txt', 'test1.txt', function (err) {
+//   console.log('Error in file moving')
+// })
+
+function down () {
+  mv('/Users/ZachO/Code/The_Archiver/tests/test1.txt', '/Users/ZachO/Code/The_Archiver/test1.txt', function (err) {
+    if (typeof err !== 'undefined') {
+        // the variable is defined
+        console.log('error on down function')
+        console.log(err.code)
+    }
+    else {
+      console.log('down function completed succesfully')
+    }
+  })
+}
+function up () {
+  mv('/Users/ZachO/Code/The_Archiver/test1.txt', '/Users/ZachO/Code/The_Archiver/tests/test1.txt', function (err) {
+    if (typeof err !== 'undefined') {
+        // the variable is defined
+        console.log('error on up function')
+        console.log(err.code)
+    }
+    else {
+      console.log('up function completed succesfully')
+    }
+  })
+}
+function translate () {
+  //translate err.code responces into plain english
+}
