875257b03a8be436850c5a368a9816e646b0a2a1
Zosman <zach.osman@gmail.com>
2017-01-15 16:36:00 -0500

    main.js work

--- main.js
+++ main.js
@@ -24,10 +24,9 @@ function down () {
   mv('/Users/ZachO/Code/The_Archiver/tests/test1.txt', '/Users/ZachO/Code/The_Archiver/test1.txt', function (err) {
     if (typeof err !== 'undefined') {
         // the variable is defined
-        console.log('error on down function')
-        console.log(err.code)
-    }
-    else {
+      console.log('error on down function')
+      console.log(translate(err.code))
+    } else {
       console.log('down function completed succesfully')
     }
   })
@@ -36,14 +35,23 @@ function up () {
   mv('/Users/ZachO/Code/The_Archiver/test1.txt', '/Users/ZachO/Code/The_Archiver/tests/test1.txt', function (err) {
     if (typeof err !== 'undefined') {
         // the variable is defined
-        console.log('error on up function')
-        console.log(err.code)
-    }
-    else {
+      console.log('error on up function')
+      console.log(translate(err.code))
+    } else {
       console.log('up function completed succesfully')
     }
   })
 }
-function translate () {
-  //translate err.code responces into plain english
+function translate (statement) {
+  // translate err.code responces into plain english
+  switch (statement) {
+    case 'ENOENT':
+      return 'ERROR: Code can not find the file to move'
+      break
+    case 'randomshit':
+      return 'kek'
+      break
+    default:
+      return ('error code "' + statement + '" is not in the translate function')
+  }
 }
