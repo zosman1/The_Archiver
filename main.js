6a575455205d6bb23bdde3af8580c30a81a31f82
Zachariah Osman <zosman@gmu.edu>
2017-03-02 23:31:46 -0500

    Changed Title, finally

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const fs = require('fs')
 const mv = require('mv')
 let lastDone = 'up' // imporant var used around main.js
-function sayHi () {
-  console.log('Hai There!')
+
+function sayHello () {
+  console.log('Hello World!')
 }
 
 const testFolder = '/Users/ZachO/Code/The_Archiver/tests'
+
 var filesList = []
 fs.readdir(testFolder, (err, files) => {
   files.forEach(file => {
@@ -13,17 +15,10 @@ fs.readdir(testFolder, (err, files) => {
     filesList.push(file)
   })
 })
-console.log(filesList)
-// mv('/Users/ZachO/Code/The_Archiver/tests/test1.txt', '/Users/ZachO/Code/The_Archiver/test1.txt', function (err) {
-//   console.log('Error in file ')
-// })
 
-// mv('/tests/test1.txt', 'test1.txt', function (err) {
-//   console.log('Error in file moving')
-// })
 function down () {
   if (lastDone == 'down') {
-    notifyUser('The Files Are Currently Down On The Local Machine.', 'blue')
+    notifyUser('The Files Are Currently Down On The Local Machine.', 'blue', 2500)
   } else {
     lastDone = 'down'
     filesList.forEach(function (file) {
@@ -38,15 +33,16 @@ function down () {
           // notifying the frontend that something has gone wrong, oops
         } else {
           console.log('down function completed succesfully on file: ' + file)
-          clearNotifications()
+          notifyUser('The File Have Succesfully Been Moved Down To The Local Machine!', 'grey', 7000)
         }
       })
     })
   }
 }
+
 function up () {
   if (lastDone == 'up') {
-    notifyUser('The Files Are Currently Up On The External Drive', 'blue')
+    notifyUser('The Files Are Currently Up On The External Drive.', 'blue', 2500)
   } else {
     lastDone = 'up'
     filesList.forEach(function (file) {
@@ -61,55 +57,71 @@ function up () {
           // notifying the frontend that something has gone wrong, oops
         } else {
           console.log('up function completed succesfully on file: ' + file)
-          clearNotifications()
+          notifyUser('The File Have Succesfully Been Moved Up To The External Drive!', 'grey', 7000)
         }
       })
     })
   }
 }
 
-function onedown () {
-  mv('/Users/ZachO/Code/The_Archiver/tests/test1.txt', '/Users/ZachO/Code/The_Archiver/test1.txt', function (err) {
-    if (typeof err !== 'undefined') {
-        // the variable is defined
-      console.log('error on down function')
-      console.log(translate(err.code))
-    } else {
-      console.log('down function completed succesfully')
-    }
-  })
-}
-function oneup () {
-  mv('/Users/ZachO/Code/The_Archiver/test1.txt', '/Users/ZachO/Code/The_Archiver/tests/test1.txt', function (err) {
-    if (typeof err !== 'undefined') {
-        // the variable is defined
-      console.log('error on up function')
-      console.log(translate(err.code))
-    } else {
-      console.log('up function completed succesfully')
-    }
-  })
-}
+// DEPRECIATED FUNCTIONS USED FOR CONCEPTUAL TESTING
+// function onedown () {
+//   mv('/Users/ZachO/Code/The_Archiver/tests/test1.txt', '/Users/ZachO/Code/The_Archiver/test1.txt', function (err) {
+//     if (typeof err !== 'undefined') {
+//         // the variable is defined
+//       console.log('error on down function')
+//       console.log(translate(err.code))
+//     } else {
+//       console.log('down function completed succesfully')
+//     }
+//   })
+// }
+//
+// function oneup () {
+//   mv('/Users/ZachO/Code/The_Archiver/test1.txt', '/Users/ZachO/Code/The_Archiver/tests/test1.txt', function (err) {
+//     if (typeof err !== 'undefined') {
+//         // the variable is defined
+//       console.log('error on up function')
+//       console.log(translate(err.code))
+//     } else {
+//       console.log('up function completed succesfully')
+//     }
+//   })
+// }
+
 function translate (statement) {
   // translate err.code responces into plain english
   switch (statement) {
     case 'ENOENT':
       return 'ERROR: Code can not find the file to move'
       break
-    case 'randomshit':
-      return 'kek'
+    case 'Zosman':
+      return 'topkek'
       break
     default:
       return ('error code "' + statement + '" is not in the translate function')
   }
 }
-// this function sends a notifcation to the main screen to notify the user of something
-function notifyUser (content, color) {
-  // color will be a #FFFFFF style input
+
+// This function sends a notifcation to the main screen to notify the user of something
+function notifyUser (content, color, time) {
+  // color will be a value acceptable by css
   // content will be the actual text
-  document.getElementById('userNotification').innerHTML = ("<h5 style = 'color:" + color + "'>" + content + '</h5>')
+  document.getElementById('userNotification').innerHTML = ("<h5 style = 'font-weight: 300; color:" + color + "'>" + content + '</h5>')
+  clearNotifications(time)
+  // for now will keep this as a seperate class
 }
-// this function will clear notifcations from the main screen
-function clearNotifications () {
-  document.getElementById('userNotification').innerHTML = ('')
+
+// This function will clear notifcations from the main screen
+function clearNotifications (time) {
+  let running = true
+  // FUTURE NOTE: will want to make notification fade out to improve ux
+  // Takes in tiem as miliseconds ex: 1000 = 1 second
+  if (running = true){
+    // present issue, runs over itself.
+    // fix: will have to kill the proccess when its reactived and its not done and start it over
+    setTimeout(function () {
+      document.getElementById('userNotification').innerHTML = ('')
+    }, time)
+  }
 }
