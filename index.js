5176420c858556769c57785ac072ee5098e7d8c7
Zachariah Osman <zosman@gmu.edu>
2017-03-04 20:15:41 -0500

    cleared all linting issues, we have finalized #12. Closes #12

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const electron = require('electron');
+const electron = require("electron");
 
 const app = electron.app;
 
 // adds debug features like hotkeys for triggering dev tools and reload
-require('electron-debug')();
+require("electron-debug")();
 
 // prevent window being garbage collected
 let mainWindow;
@@ -11,33 +11,33 @@ let mainWindow;
 function onClosed() {
   // dereference the window
   // for multiple windows store them in an array
-  mainWindow = null;
+	mainWindow = null;
 }
 
 function createMainWindow() {
-  const win = new electron.BrowserWindow({
-    width: 600,
-    height: 400,
-  });
+	const win = new electron.BrowserWindow({
+		width: 600,
+		height: 400,
+	});
 
-  win.loadURL(`file://${__dirname}/index.html`);
-  win.on('closed', onClosed);
+	win.loadURL(`file://${__dirname}/index.html`);
+	win.on("closed", onClosed);
 
-  return win;
+	return win;
 }
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
 });
 
-app.on('activate', () => {
-  if (!mainWindow) {
-    mainWindow = createMainWindow();
-  }
+app.on("activate", () => {
+	if (!mainWindow) {
+		mainWindow = createMainWindow();
+	}
 });
 
-app.on('ready', () => {
-  mainWindow = createMainWindow();
+app.on("ready", () => {
+	mainWindow = createMainWindow();
 });
--- main.js
+++ main.js
@@ -1,95 +1,95 @@
-const fs = require('fs-extra');
+const fs = require("fs-extra");
 
-let lastDone = 'up'; // imporant var used around main.js
+let lastDone = "up"; // imporant var used around main.js
 
 // eslint-disable-next-line no-unused-vars
 function sayHello() {
-  console.log('Hello World!');
+	console.log("Hello World!");
 }
 
-const awayFolder = '/Volumes/Samsung_T3/Test_Folder/';
-const homeFolder = '/Users/ZachO/Code/The_Archiver/vmTests/';
+const awayFolder = "/Volumes/Samsung_T3/Test_Folder/";
+const homeFolder = "/Users/ZachO/Code/The_Archiver/vmTests/";
 
 const filesList = [];
 fs.readdir(awayFolder, (err, files) => {
-  files.forEach((file) => {
-    console.log(file);
-    filesList.push(file);
-  });
+	files.forEach((file) => {
+		console.log(file);
+		filesList.push(file);
+	});
 });
 
 function down() {
-  if (lastDone == 'down') {
-    notifyUser('The Files Are Currently Down On The Local Machine.', 'blue', 2500);
-  } else {
-    lastDone = 'down';
-    filesList.forEach((file) => {
-      fs.move(awayFolder + file, homeFolder + file, (err) => {
-        if (err != null) {
+	if (lastDone == "down") {
+		notifyUser("The Files Are Currently Down On The Local Machine.", "blue", 2500);
+	} else {
+		lastDone = "down";
+		filesList.forEach((file) => {
+			fs.move(awayFolder + file, homeFolder + file, (err) => {
+				if (err != null) {
             // the variable is defined
-          console.log(`error on down function file: ${file}`);
+					console.log(`error on down function file: ${file}`);
           // printing what file this failed on
-          console.log(err);
+					console.log(err);
           // printing the error code
-          notifyUser('Down function has failed', 'red');
+					notifyUser("Down function has failed", "red");
           // notifying the frontend that something has gone wrong, oops
-        } else {
-          console.log(`down function completed succesfully on file: ${file}`);
-          notifyUser('The File Have Succesfully Been Moved Down To The Local Machine!', 'grey', 7000);
-        }
-      });
-    });
-  }
+				} else {
+					console.log(`down function completed succesfully on file: ${file}`);
+					notifyUser("The File Have Succesfully Been Moved Down To The Local Machine!", "grey", 7000);
+				}
+			});
+		});
+	}
 }
 
 function up() {
-  if (lastDone == 'up') {
-    notifyUser('The Files Are Currently Up On The External Drive.', 'blue', 2500);
-  } else {
-    lastDone = 'up';
-    filesList.forEach((file) => {
-      fs.move(homeFolder + file, awayFolder + file, (err) => {
-        if (err != null) {
+	if (lastDone == "up") {
+		notifyUser("The Files Are Currently Up On The External Drive.", "blue", 2500);
+	} else {
+		lastDone = "up";
+		filesList.forEach((file) => {
+			fs.move(homeFolder + file, awayFolder + file, (err) => {
+				if (err != null) {
             // the variable is defined
-          console.log(`error on down function file: ${file}`);
+					console.log(`error on down function file: ${file}`);
           // printing what file this failed on
-          console.log(err);
+					console.log(err);
           // printing the error code
-          notifyUser('Up function has failed', 'red');
+					notifyUser("Up function has failed", "red");
           // notifying the frontend that something has gone wrong, oops
-        } else {
-          console.log(`up function completed succesfully on file: ${file}`);
-          notifyUser('The File Have Succesfully Been Moved Up To The External Drive!', 'grey', 7000);
-        }
-      });
-    });
-  }
+				} else {
+					console.log(`up function completed succesfully on file: ${file}`);
+					notifyUser("The File Have Succesfully Been Moved Up To The External Drive!", "grey", 7000);
+				}
+			});
+		});
+	}
 }
 
 // This function sends a notifcation to the main screen to notify the user of something
 function notifyUser(content, color, time) {
   // color will be a value acceptable by css
   // content will be the actual text
-  document.getElementById('userNotification').innerHTML = (`<h5 style = 'font-weight: 400; color:${color}'>${content}</h5>`);
-  clearNotifications(time);
+	document.getElementById("userNotification").innerHTML = (`<h5 style = 'font-weight: 400; color:${color}'>${content}</h5>`);
+	clearNotifications(time);
   // for now will keep this as a seperate class
 }
 
 exports.notifyUser = function nU(content, color, time) {
-  notifyUser(content, color, time);
+	notifyUser(content, color, time);
 };
 
 // This function will clear notifcations from the main screen
 function clearNotifications(time) {
-  let running = true;
+	let running = true;
   // FUTURE NOTE: will want to make notification fade out to improve ux
   // Takes in tiem as miliseconds ex: 1000 = 1 second
-  if (running = true) {
+	if (running == true) {
     // currently doesent do anything
     // present issue, runs over itself.
     // fix: will have to kill the proccess when its reactived and its not done and start it over
-    setTimeout(() => {
-      document.getElementById('userNotification').innerHTML = ('');
-    }, time);
-  }
+		setTimeout(() => {
+			document.getElementById("userNotification").innerHTML = ("");
+		}, time);
+	}
 }
