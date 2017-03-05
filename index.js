88db9689c8dea1f7152e90635edc387636a5fc3d
Zachariah Osman <zosman@gmu.edu>
2017-03-04 23:01:47 -0500

    added an attempt at a logo, not great with design, but hey it works! (will probably redo later in project) Closes #15

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ function createMainWindow() {
 	const win = new electron.BrowserWindow({
 		width: 600,
 		height: 400,
+		// eslint-disable-next-line no-undef
+		icon: path.join(__dirname, "assets/icons/png/64x64.png"),
 	});
 
 	win.loadURL(`file://${__dirname}/index.html`);
