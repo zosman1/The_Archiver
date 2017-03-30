0ec38e2385b5e6348daccaba810ae5744a3c12ef
Zachariah Osman <zosman@gmu.edu>
2017-03-30 19:31:32 -0400

    removed unnessacary eslint exception

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ function createMainWindow() {
 	const win = new electron.BrowserWindow({
 		width: 600,
 		height: 400,
-		// eslint-disable-next-line no-undef
 		icon: path.join(__dirname, "assets/icons/png/64x64.png")
 	});
 
