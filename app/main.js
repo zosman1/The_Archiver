d5d180a15b8deacb3091855161362932634c7c64
Zach Osman <zosman@gmu.edu>
2017-04-07 00:32:53 -0400

    Completed Fixes for #27 Have reworked my functions improved code efficiency better delegated tasks to dedicated functions - Zosman

--- app/main.js
+++ app/main.js
@@ -6,87 +6,56 @@ const ipc = require("electron").ipcRenderer;
 let ready = false;
 let filelocation = "up";
 const debug = true;
+
 function init() {
-	return new Promise((resolve, reject) => {
-		if (settings.has("path.home") && settings.has("path.away")) {
-			fs.readdir(settings.get("path.away"), (err, files) => {
-				if (err) {
-					reject(err);
-					return;
-				}
-				resolve(files);
-			});
-			ready = true;
-		}
-	});
+	if (settings.has("path.home") && settings.has("path.away")) {
+		ready = true;
+	}
 }
-let files;
-init().then(res => {
-	files = res;
-}).catch(err => {
-	console.error(err);
-});
-// eslint-disable-next-line no-unused-vars
-function down() {
-	if (ready){
-		if (filelocation == "down") {
-			notifyUser("The Virtual Machine is currently down on the local machine!", "lightblue", 3000);
-		} else {
-			filelocation = "down";
-			let ifError = false;
-			files.forEach((file) => {
-				fs.move(path.join(settings.get("path.away"), file), path.join(settings.get("path.home"), file), (err) => {
-					if (err != null) {
-                    // Error callback
-						if (debug) {
-							console.log(" ");
-							console.log(err);
-							console.log(`Error on down function, File: ${file}`);
-							console.log(" ");
-						}
-						ifError = true;
-					}
-				});
-			});
-			if (!ifError) {
-				notifyUser("The Virtual Machine Has Been Moved Down Onto The Local Machine!", "darkgrey", 7000);
-			} else if (debug) {
-				ipc.send("error-down");
-			}
+init();
+
+ // eslint-disable-next-line no-unused-vars
+function moveFiles(direction){
+	let fileArgs = {
+		up: ["path.home", "path.away"],
+		down: ["path.away", "path.home"]
+	};
+	if (!ready) return;
+	if (direction != filelocation){
+		filelocation = direction;
+		fs.move(settings.get(fileArgs[direction][0]), settings.get(fileArgs[direction][1]), (err) => {
+			errorHandle(err, direction);
+		});
+	}		
+}
+
+ // eslint-disable-next-line no-unused-vars
+function errorHandle(err, direction){
+	let notifyMessage = {
+		down: ["The Virtual Machine Has Been Moved Down Onto The Local Drive!", "darkgrey", 7000],
+		up: ["The Virtual Machine Has Been Moved Up Onto The External Drive!", "darkgrey", 7000]
+	};
+	let ipcMessage = {
+		down: "error-down",
+		up: "error-up"
+	};
+	let ifError = false;
+	if (err) {
+		// Error callback
+		if (debug) {
+			console.error(`Error on moving ${direction} function: ${err}`);
+			ipc.send(ipcMessage[direction]);
+		}
+		ifError = true;
+	}
+	else {
+		if (!ifError) {
+			notifyUser(notifyMessage[direction][0],notifyMessage[direction][1],notifyMessage[direction][2]);
 		}
 	}
 }
-// eslint-disable-next-line no-unused-vars
-function up() {
-	if (ready){
-		if (filelocation == "up") {
-			notifyUser("The Virtual Machine is currently on the local machine!", "lightblue", 3000);
-		} else {
-			filelocation = "up";
-			let ifError = false;
-			files.forEach((file) => {
-				fs.move(path.join(settings.get("path.home"), file), path.join(settings.get("path.away"), file), (err) => {
-					if (err != null) {
-                    // Error callback
-                    // electronerror()
-						if (debug) {
-							console.log(" ");
-							console.log(err);
-							console.log(`Error on up function, File: ${file}`);
-							console.log(" ");
-						}
-						ifError = true;
-					}
-				});
-			});
-			if (!ifError) {
-				notifyUser("The Virtual Machine Has Been Moved Up Onto The External Drive!", "darkgrey", 7000);
-			} else if (debug) {
-				ipc.send("error-up");
-			}
-		}
-	}
-}
+
+// Notify users via HTML Functions
 let runningNotifications = [];
 function notifyUser(content, color, time) {
 	runningNotifications.push([content, color]);
@@ -108,3 +77,4 @@ function updateDisplay() {
 		notifElem.innerHTML = ("");
 	}
 }
+// End notify via HTML functions
