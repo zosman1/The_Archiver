c5cf53b176bfe032ebd988b7783bdb9ea50960b6
Zach Osman <zosman@gmu.edu>
2017-05-01 23:38:39 -0400

    Adding in the airbnb style This will improve code readability and allow for better community understanding of what my code does
    Closes #35
    - Zosman

--- app/settings.js
+++ app/settings.js
@@ -1,17 +1,17 @@
-const settings = require("electron-settings");
-const main = require("./main.js");
+const settings = require('electron-settings');
+const main = require('./main.js');
 
 // eslint-disable-next-line no-unused-vars
 function submitSettings() {
-	const homePath = document.getElementById("homePath").value;
-	const awayPath = document.getElementById("awayPath").value;
-	settings.set("path", {
-		home: homePath,
-		away: awayPath,
-	});
-	document.getElementById("homePath").value = "";
-	document.getElementById("awayPath").value = "";
-	main.notifyUser("Settings Succesfully Changed!", "lightblue", 5000);
+  const homePath = document.getElementById('homePath').value;
+  const awayPath = document.getElementById('awayPath').value;
+  settings.set('path', {
+    home: homePath,
+    away: awayPath,
+  });
+  document.getElementById('homePath').value = '';
+  document.getElementById('awayPath').value = '';
+  main.notifyUser('Settings Succesfully Changed!', 'lightblue', 5000);
 }
 
 console.log(settings.getAll());
